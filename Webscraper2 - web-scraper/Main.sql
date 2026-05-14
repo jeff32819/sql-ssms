c70506ce29724018dbd05a774255cded16012622
Jeff Mathews <jeff32819@gmail.com>
2026-05-14 02:17:27 +0000

    Update Main.sql

--- Webscraper2 - web-scraper/Main.sql
+++ Webscraper2 - web-scraper/Main.sql
@@ -1,6 +1,13 @@
 use v2
 go
 
+SELECT 
+    (SELECT COUNT(*) FROM [linkHashTbl]) AS LinkHashCount,
+    (SELECT COUNT(*) FROM [linkTbl]) AS LinkCount,
+    (SELECT COUNT(*) FROM [pageTbl]) AS PageCount;
+
+
+
 SELECT * FROM [linkHashTbl]
 SELECT * FROM [linkTbl]
 SELECT * FROM [pageTbl]
