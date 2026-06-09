007ff0b069e5416d8869d14879fb68047f8acfa2
Jeff Mathews <jeff32819@gmail.com>
2026-06-09 17:30:13 +0000

    Update Restore backup from IONOS.sql

--- Jamin/Restore backup from IONOS.sql
+++ Jamin/Restore backup from IONOS.sql
@@ -1,5 +1,5 @@
 
--- Generated UTC : [2026-06-05 18:24:57] 
+-- Generated UTC : [2026-06-09 17:28:51] 
 -- Target Server : [PANAMACITY\DEV14]
 -- Database      : [Jamin]
 
@@ -20,7 +20,7 @@ DECLARE @LogsName NVARCHAR(128) = N'Jamin_log';
 -- Physical Paths
 DECLARE @DataFilePath NVARCHAR(500) = N'X:\SqlServerData\Dev14\Jamin_data.mdf';
 DECLARE @LogsFilePath NVARCHAR(500) = N'X:\SqlServerData\Dev14\Jamin_logs.ldf';
-DECLARE @BakFilePath  NVARCHAR(500) = N'X:\SqlServerBak\Dev14\JaminFromIONOS\069.burks_decimal_fix__26-06-05__1422~[JEFF32819VM01~jsm14]~[Jamin].bak';
+DECLARE @BakFilePath  NVARCHAR(500) = N'X:\SqlServerBak\Dev14\JaminFromIONOS\071.trucktrackertbl__26-06-09__1324~[JEFF32819VM01~jsm14]~[Jamin].bak';
 
 -- =============================================
 -- 2. SERVER SAFETY CHECK
