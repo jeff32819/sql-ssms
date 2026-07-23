f8704b55651843d970cf87ae68779958b6da9b02
Jeff Mathews <jeff32819@gmail.com>
2026-07-23 20:18:27 +0000

    misc

--- Jamin/Estimators.sql
+++ Jamin/Estimators.sql
@@ -2,3 +2,20 @@ DECLARE @quoteID INT = 64518;
 
 select * from quoteTbl where quoteID = @quoteID
 select * from quoteEstimateDateTbl where quoteID = @quoteID
+
+select * from empTbl where empID in (select estimatorId from quoteEstimateDateTbl where quoteID = @quoteID)
+
+
+
+go
+alter view estimatorReportQry as
+select 
+	d.estimateDateId,
+	d.quoteId,
+	d.estimatorId,
+	e.empFirstName,
+	e.empLastName,
+	d.dateTime as estimateDateTime
+from quoteEstimateDateTbl d
+inner join empTbl e on e.empID = d.estimatorId
+where d.estimatorId > 0
