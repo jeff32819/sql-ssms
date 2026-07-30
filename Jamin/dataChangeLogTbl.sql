5bf6532d20f733ac60f6db76f9b8d4c6cac91149
Jeff Mathews <jeff32819@gmail.com>
2026-07-30 15:08:22 +0000

    misc

--- Jamin/dataChangeLogTbl.sql
+++ Jamin/dataChangeLogTbl.sql
@@ -1,15 +1,25 @@
-SELECT TOP (1000) [id]
-      ,[title]
-      ,[text]
-      ,[employeeId]
-      ,[quoteId]
-      ,[dateTimeStamp]
-      ,[tableName]
-      ,[fieldName]
-      ,[fieldType]
-      ,[oldValue]
-      ,[newValue]
-      ,[fieldUid]
-      ,[jsonData]
-  FROM [Jamin].[dbo].[dataChangeLogTbl]
-ORDER BY [dateTimeStamp] DESC
+-- delete from [dataChangeLogTbl]
+
+select * from dataChangeLogQry
+
+
+
+
+
+
+--SELECT TOP (1000) [id]
+--      ,[title]
+--      ,[text]
+--      ,[employeeId]
+--      ,[quoteId]
+--      ,[dateTimeStamp]
+--      ,[tableName]
+--      ,[fieldName]
+--      ,[fieldType]
+--      ,[oldValue]
+--      ,[newValue]
+--      ,[fieldUid]
+--      ,[jsonData]
+--  FROM [Jamin].[dbo].[dataChangeLogTbl]
+--ORDER BY [dateTimeStamp] DESC
+--go 
