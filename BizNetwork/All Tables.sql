718532ab5896b6c720e2338bcae6ee885c87983a
Jeff Mathews <jeff32819@gmail.com>
2026-08-05 19:31:35 +0000

    misc

---
+++ BizNetwork/All Tables.sql
@@ -0,0 +1,4 @@
+SELECT * FROM [BizNetwork].[dbo].[contactTbl]
+SELECT * FROM [BizNetwork].[dbo].[companyTbl]
+SELECT * FROM [BizNetwork].[dbo].[websiteTbl]
+SELECT * FROM [BizNetwork].[dbo].[noteTbl]
